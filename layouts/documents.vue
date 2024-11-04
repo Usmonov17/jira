45f98f4ef7ba8362cde8a3a64ad9c04897f85491
Developer1702 <usmonov1702@gmail.com>
2024-11-04 17:38:34 +0500

    More changes

--- layouts/documents.vue
+++ layouts/documents.vue
@@ -4,7 +4,7 @@
     <LayoutsDashboard />
     <section class="min-h-screen bg-white dark:bg-black pl-[20%] pt-16">
       <div class="p-4">
-        <router-view />
+        <router-view /> 
       </div>
     </section>
   </main>
